c6913550fdf3627788dc09ee3d163473e003ac70
Babatunde99 <adeboyebabatunde93@gmail.com>
2022-03-14 14:18:16 +0100

    changes made to javascript file

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log(z+x)
+
+let c = 1;
+let b = 8;
+let u = c + b;
+console.log(u)
+
+
+let num1 =9, num2=3;
+console.log("*********")
+console.log(num1 * num2)
+console.log(num1 - num2)
+console.log(num1 + num2)
+console.log(num1 / num2)
+console.log(num1 ** num2)
+console.log(num1 % num2)
+
+
+
 let userName = prompt("Enter your name:");
 document.getElementById("demo").innerHTML = userName;
 
