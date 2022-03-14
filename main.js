1077073219787abb08910c21b7d6a00473c4ef27
Babatunde99 <adeboyebabatunde93@gmail.com>
2022-03-14 12:51:41 +0100

    first assignment

---
+++ main.js
@@ -0,0 +1,12 @@
+
+let userName = prompt("Enter your name:");
+document.getElementById("demo").innerHTML = userName;
+
+let userAge = prompt("Enter your age:");
+call.innerHTML = userAge;
+
+let userAddress = prompt("Enter your address:");
+action.innerHTML = userAddress;
+
+let userNumber = prompt("Enter your Phone number:");
+option.innerHTML = userNumber;
