c1f7d9790b5f050f960a845c8efeeaba50d8bf3c
Babatunde99 <adeboyebabatunde93@gmail.com>
2022-03-14 15:52:27 +0100

    assignment done

--- main.js
+++ main.js
@@ -1,38 +1,49 @@
 
-var x = 5;
-var y = 6;
-var z = x + y;
-console.log(z+x)
+// var x = 5;
+// var y = 6;
+// var z = x + y;
+// console.log(z+x)
 
-let c = 1;
-let b = 8;
-let u = c + b;
-console.log(u)
-
-
-let num1 =9, num2=3;
-console.log("*********")
-console.log(num1 * num2)
-console.log(num1 - num2)
-console.log(num1 + num2)
-console.log(num1 / num2)
-console.log(num1 ** num2)
-console.log(num1 % num2)
+// let c = 1;
+// let b= 8;
+// let u = c + b;
+// console.log(u)
 
 
+// let num1 =9, num2=3;
+// console.log("*********")
+// console.log(num1 * num2)
+// console.log(num1 - num2)
+// console.log(num1 + num2)
+// console.log(num1 / num2)
+// console.log(num1 ** num2)
+// console.log(num1 % num2)
 
-let userName = prompt("Enter your name:");
-document.getElementById("demo").innerHTML = userName;
 
-let userAge = prompt("Enter your age:");
-call.innerHTML = userAge;
 
-let userAddress = prompt("Enter your address:");
-action.innerHTML = userAddress;
+// let userName = prompt("Enter your name:");
+// document.getElementById("demo").innerHTML = userName;
 
-let userNumber = prompt("Enter your Phone number:");
-option.innerHTML = userNumber;
+// let userAge = prompt("Enter your age:");
+// call.innerHTML = userAge;
 
-alert("welcome to JavaScript " + userName)
+// let userAddress = prompt("Enter your address:");
+// action.innerHTML = userAddress;
+
+// let userNumber = prompt("Enter your Phone number:");
+// option.innerHTML = userNumber;
+
+// alert("welcome to JavaScript " + userName)
 
 // write a program with javascript to calculate the number of seconds in a week then display the answer in the html page
+
+let sec = 60;
+let hour = 60 * sec;
+let day = 24;
+let o = 24 * hour;
+let week = o * 7
+console.log(week)
+// document.getElementById("weeks").innerHTML = week
+weeks.innerHTML = week
+
+
