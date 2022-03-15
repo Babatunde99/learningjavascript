1c96ea2204d8dad35f0c72d80e59944bd1182d16
Babatunde99 <adeboyebabatunde93@gmail.com>
2022-03-15 14:13:54 +0100

    ....

--- main.js
+++ main.js
@@ -1,41 +1,40 @@
 
-// var x = 5;
-// var y = 6;
-// var z = x + y;
-// console.log(z+x)
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log(z+x)
 
-// let c = 1;
-// let b= 8;
-// let u = c + b;
-// console.log(u)
-
-
-// let num1 =9, num2=3;
-// console.log("*********")
-// console.log(num1 * num2)
-// console.log(num1 - num2)
-// console.log(num1 + num2)
-// console.log(num1 / num2)
-// console.log(num1 ** num2)
-// console.log(num1 % num2)
+let c = 1;
+let b= 8;
+let u = c + b;
+console.log(u)
 
 
+let num1 =9, num2=3;
+console.log("*********")
+console.log(num1 * num2)
+console.log(num1 - num2)
+console.log(num1 + num2)
+console.log(num1 / num2)
+console.log(num1 ** num2)
+console.log(num1 % num2)
 
-// let userName = prompt("Enter your name:");
-// document.getElementById("demo").innerHTML = userName;
 
-// let userAge = prompt("Enter your age:");
-// call.innerHTML = userAge;
 
-// let userAddress = prompt("Enter your address:");
-// action.innerHTML = userAddress;
+let userName = prompt("Enter your name:");
+document.getElementById("demo").innerHTML = userName;
 
-// let userNumber = prompt("Enter your Phone number:");
-// option.innerHTML = userNumber;
+let userAge = prompt("Enter your age:");
+call.innerHTML = userAge;
 
-// alert("welcome to JavaScript " + userName)
+let userAddress = prompt("Enter your address:");
+action.innerHTML = userAddress;
 
-// write a program with javascript to calculate the number of seconds in a week then display the answer in the html page
+let userNumber = prompt("Enter your Phone number:");
+option.innerHTML = userNumber;
+
+alert("welcome to JavaScript " + userName)
+
 
 let sec = 60;
 let hour = 60 * sec;
@@ -43,7 +42,7 @@ let day = 24;
 let o = 24 * hour;
 let week = o * 7
 console.log(week)
-// document.getElementById("weeks").innerHTML = week
 weeks.innerHTML = week
+// document.getElementById("weeks").innerHTML = week
 
 
